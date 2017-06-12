ec04799a200e0a45cbc91a433b62d6481bf6a21e
j-b-z <jbz@andrew.cmu.edu>
2017-06-12 10:57:16 -0400

    added basic stuff; it now tests get child support

--- Documents/SOAPandZEEP.git/client skeleton jbz.py
+++ Documents/SOAPandZEEP.git/client skeleton jbz.py
@@ -6,21 +6,20 @@ client = zeep.Client(wsdl=wsdl)
 login_info = client.get_type('ns0:loginInfo')
 log_in = login_info(firmId=2, loginId='test', password='isdn1')
 
-leinName = client.get_type('ns0:stateWideLienName')
-test_name = leinName(SearchExactName='False', )
+CSName = client.get_type('ns0:childSupportName')
+test_name = CSName(searchExactName='False', firstName="TEST", lastName="EXAMPLE", fromDate='01/01/2010', toDate='01/01/2017')
+
+nameList = [test_name]
+
+childSupportOrder = client.get_type('ns0:childSupportOrder')
+test_CSO = childSupportOrder(deliveryMethod='Email', deliveryInfo='JZhang@signatureinfo.com', vendorId=2, clientFirmNumber='1234', name=nameList )
 
 stateWideLienOrder = client.get_type('ns0:stateWideLienOrder')
-test_sWLO = stateWideLienOrder(deliveryMethod='Email', deliveryInfo='JZhang@signatureinfo.com', reference='AMIT', vendorId=9999,
-                                )
-
-     ns0:stateWideLienOrder(appendToConfirmationNumber: xsd:string, clientFirmNu
-mber: xsd:string, deliveryInfo: xsd:string, deliveryMethod: xsd:string, entryUse
-r: xsd:string, name: ns0:stateWideLienName[], parentConfirmationNumber: xsd:stri
-ng, reference: xsd:string, source: xsd:string, vendorId: xsd:long, vendorTransac
-tionId: xsd:string)
+test_sWLO = stateWideLienOrder(deliveryMethod='Web', reference='JEREMY', vendorId=9999)
 
 print(client.service.login(log_in))
 print(client.service.getStateWideLienEffectiveDate())
+print(client.service.submitChildSupport(test_CSO))
 
 
 client.service.logout()
