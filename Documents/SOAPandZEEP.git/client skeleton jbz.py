eaa2eaf98952ed3449d12a2e3ab8cf2981505c39
j-b-z <jbz@andrew.cmu.edu>
2017-06-14 13:20:25 -0400

    added tideland
    waiting feedback for test

--- Documents/SOAPandZEEP.git/client skeleton jbz.py
+++ Documents/SOAPandZEEP.git/client skeleton jbz.py
@@ -1,6 +1,11 @@
 import zeep
+import openpyxl
 
-wsdl = 'http://alalnsis008.risk.regn.net:8080/ws/signon?wsdl'
+select = 2   #0 = alalnsis, 1 = dev, 2 = test
+endpoint = ['http://alalnsis008.risk.regn.net:8080/ws/signon?wsdl',
+            'http://sis-jas-dev.risk.regn.net:8080/ws/signon?wsdl',
+            'http://sis-jas-test.risk.regn.net:8080/ws/signon?wsdl']
+wsdl = endpoint[select]
 client = zeep.Client(wsdl=wsdl)
 
 login_info = client.get_type('ns0:loginInfo')
@@ -9,30 +14,40 @@ log_in = login_info(firmId=2, loginId='test', password='isdn1')
 CSName = client.get_type('ns0:childSupportName')
 test_name  = CSName(searchExactName=False, firstName="LOREEN", lastName="DOE", fromDate='09/01/1992', toDate='08/01/2016')
 test_name2 = CSName(searchExactName=False, firstName="KAREN", lastName="CANE", fromDate='09/01/1992', toDate='08/01/2016')
-test_name3 = CSName(searchExactName=False, firstName="JANE", lastName="BROWN", fromDate='09/01/1992', toDate='08/01/2016')
+test_name3 = CSName(searchExactName=False, firstName="JANE", lastName="BRAUN", fromDate='09/01/1992', toDate='08/01/2016')
 
-nameList = [test_name]
+names = [test_name, test_name2, test_name3]
+nameList = [names[select]]
 
 childSupportOrder = client.get_type('ns0:childSupportOrder')
 patriotOrder = client.get_type('ns0:patriotOrder')
 lienOrder = client.get_type('ns0:stateWideLienOrder')
+tideOrder = client.get_type('ns0:tidelandOrder')
 
 print(client.service.login(log_in))
 #client.service.logout()
 print(client.service.getStateWideLienEffectiveDate())
 
+ref = ['A_PYTHON', 'D_PYTHON', 'T_PYTHON']
+
 i = 99
 test_CSO = childSupportOrder(deliveryMethod='email', deliveryInfo='JZhang@signatureinfo.com', 
-	                         reference='A_PYTHON_CSO', vendorId=i, name=nameList )
-print(client.service.submitChildSupport(test_CSO))
+	                         reference=ref[select]+'_CSO', vendorId=i, name=nameList )
 
 test_Patriot =  patriotOrder(deliveryMethod='email', deliveryInfo='JZhang@signatureinfo.com', 
-	                         reference='A_PYTHON_PT',  vendorId=i, name=nameList )
-print(client.service.submitPatriot(test_Patriot))
+	                         reference=ref[select]+'_PT',  vendorId=i, name=nameList )
 
 test_SWLO =        lienOrder(deliveryMethod='email', deliveryInfo='JZhang@signatureinfo.com', 
-	                         reference='A_PYTHON_LO',  vendorId=i, name=nameList )
+	                         reference=ref[select]+'_LO',  vendorId=i, name=nameList )
 
-print(client.service.submitStateWideLien(test_SWLO))
+test_TLO =         tideOrder(deliveryMethod='email', deliveryInfo='JZhang@signatureinfo.com',
+	                         reference=ref[select]+'_TLO', vendorId=i, county='CUMBERLAND',
+	                         block=1, lot=1, municipality='GREENWICH', municipalityType='TOWNSHIP',
+	                         owner='JOHN KANE', state='NJ', hasGrantPackagePricing=True, isGrantRequested=True)
 
+#print(client.service.submitPatriot(test_Patriot)) #test submitPatriot
+#print(client.service.submitChildSupport(test_CSO)) #test submitChildSupport
+#print(client.service.submitStateWideLien(test_SWLO)) #test submitStateWideLien
+print(client.service.submitTideland(test_TLO))
+print(client.service.submit)
 client.service.logout()
